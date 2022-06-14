2d1cf8a7c798106fe1163c9e13988e280250d419
Елизавета Махоткина <emakhotkina@kameleoon.com>
2022-06-14 18:58:18 +0300

    start lean states

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import Message from './Message';
+import React, { useState } from 'react';
+
 
 function App() {
   const name = 'lizon';
+  const [messageList, setMessageList] = useState([]);
+  const [text, setText] = useState('');
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Message name={name} />
+        <div>
+          <input type="text" />
+          {messageList.map(item => {
+            return (
+              <div key={item.id}>
+                {item.text}
+              </div>
+            )
+          })}
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,8 +33,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </header >
+    </div >
   );
 }
 
