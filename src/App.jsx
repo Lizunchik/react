eb6fd17651f0b9119f89f8349a82b130c5bbf679
Елизавета Махоткина <emakhotkina@kameleoon.com>
2022-06-27 11:02:18 +0300

    change branch

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,26 @@ import ListSubheader from '@mui/material/ListSubheader';
 import List from '@mui/material/List';
 import { Box, TextField } from '@mui/material';
 import Chats from './Chats';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProfilePage from './pages/ProfilePage';
 import ChatPage from './pages/ChatPage';
 import NotFoundPage from './pages/NotFoundPage';
-
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
 
+  const count = useSelector(state => state.count);
+  const dispatch = useDispatch();
   return (
-    <Routes>
-      <Route path={'/'} element={<HomePage/>} />
-      <Route path={'/chats'} element={<ChatPage/>} />
-      <Route path={'/profile'} element={<ProfilePage/>} />
+    <div>
+      <button onClick={() => dispatch({ type: 'plus' })}>+</button>
+      <div>{count}</div>
+      <button onClick={() => dispatch({ type: 'minus' })}>-</button>
+      <button onClick={() => dispatch({ type: 'reset' })}>reset</button>
+    </div>
 
-      <Route path={'*'} element={<NotFoundPage/>} />
 
-    </Routes>
-    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,14 +4,46 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
+const initialState = {
+    count: 0
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'plus':
+            return {
+                ...state,
+                count: state.count + 1
+            }
+
+        case 'minus':
+            return {
+                ...state,
+                count: state.count - 1
+            }
+        case 'reset':
+            return {
+                ...state,
+                count: state.count = 0
+            }
+        default:
+            return state;
+    }
+}
+const store = createStore(reducer);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </BrowserRouter>
+    </Provider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
