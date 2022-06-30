73ca7f59f0f29a172ba8e6926ad33870b8fce0e6
Елизавета Махоткина <emakhotkina@kameleoon.com>
2022-06-30 13:41:18 +0300

    lesson 6

--- src/index.js
+++ src/index.js
@@ -1,52 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
+import {store} from './redux/configureStore';
 
-const initialState = {
-    count: 0
-}
-
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'plus':
-            return {
-                ...state,
-                count: state.count + 1
-            }
-
-        case 'minus':
-            return {
-                ...state,
-                count: state.count - 1
-            }
-        case 'reset':
-            return {
-                ...state,
-                count: state.count = 0
-            }
-        default:
-            return state;
-    }
-}
-const store = createStore(reducer);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
-        <BrowserRouter>
+    <BrowserRouter>
+        <Provider store={store}>
             <React.StrictMode>
                 <App />
             </React.StrictMode>
-        </BrowserRouter>
-    </Provider>
+        </Provider>
+
+    </BrowserRouter>
+
 
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
